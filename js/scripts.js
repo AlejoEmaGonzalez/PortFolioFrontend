22900a3e8bab76d84472c5ef64e49ec923d723df
Emanuel <ema_alejo01@hotmail.com>
2022-09-30 14:26:37 -0300

    Frontend portfilo

---
+++ js/scripts.js
@@ -0,0 +1,111 @@
+/* Funciones para editar nombre*/
+function edit(){
+    document.getElementById("text-nombre").contentEditable=true;   
+    document.getElementById("edit-text-nom").hidden=true;
+    document.getElementById("edit-text-nom-ok").hidden=false;  
+}
+function editOk(){
+    document.getElementById("text-nombre").contentEditable=false;   
+    document.getElementById("edit-text-nom").hidden=false;
+    document.getElementById("edit-text-nom-ok").hidden=true;
+}
+/*----------------------------*/
+/* Funciones para editar dregree*/
+function edit_degree(){
+    document.getElementById("text-degree").contentEditable=true;   
+    document.getElementById("edit-text-degree").hidden=true;
+    document.getElementById("edit-text-degree-ok").hidden=false;  
+}
+function edit_degreeOk(){
+    document.getElementById("text-degree").contentEditable=false;   
+    document.getElementById("edit-text-degree").hidden=false;
+    document.getElementById("edit-text-degree-ok").hidden=true;
+}
+/*----------------------------*/
+/* Funciones para editar home*/
+function edit_home(){
+    document.getElementById("text-home").contentEditable=true;
+    document.getElementById("edit-text-home").hidden=true;
+    document.getElementById("edit-text-home-ok").hidden=false;
+}
+function edit_homeOk(){
+    document.getElementById("text-home").contentEditable=false;   
+    document.getElementById("edit-text-home").hidden=false;
+    document.getElementById("edit-text-home-ok").hidden=true;
+}
+/*----------------------------*/
+/* Funciones para editar Acerca de*/
+function edit_acercade(){
+    document.getElementById("text-acercade").contentEditable=true;   
+    document.getElementById("edit-text-acercade").hidden=true;
+    document.getElementById("edit-text-acercade-ok").hidden=false;  
+}
+function edit_acercadeOk(){
+    document.getElementById("text-acercade").contentEditable=false;   
+    document.getElementById("edit-text-acercade").hidden=false;
+    document.getElementById("edit-text-acercade-ok").hidden=true;
+}
+function elim_acercade(){
+    document.getElementById("acercade").hidden=true;
+}
+/*----------------------------*/
+/* Funciones para editar Experiencia*/
+function edit_exp(){
+    document.getElementById("text-exp").contentEditable=true;   
+    document.getElementById("edit-text-exp").hidden=true;
+    document.getElementById("edit-text-exp-ok").hidden=false;  
+}
+function edit_expOk(){
+    document.getElementById("text-exp").contentEditable=false;   
+    document.getElementById("edit-text-exp").hidden=false;
+    document.getElementById("edit-text-exp-ok").hidden=true;
+}
+function elim_exp(){
+    document.getElementById("exp").hidden=true;
+}
+/*----------------------------*/
+/* Funciones para editar Educacion*/
+function edit_educ(){
+    document.getElementById("text-educ").contentEditable=true;   
+    document.getElementById("edit-text-educ").hidden=true;
+    document.getElementById("edit-text-educ-ok").hidden=false;  
+}
+function edit_educOk(){
+    document.getElementById("text-educ").contentEditable=false;   
+    document.getElementById("edit-text-educ").hidden=false;
+    document.getElementById("edit-text-educ-ok").hidden=true;
+}
+function elim_educ(){
+    document.getElementById("educ").hidden=true;
+}
+/*----------------------------*/
+/* Funciones para editar Hard & Soft Skills*/
+function edit_skills(){
+    document.getElementById("text-skills").contentEditable=true;   
+    document.getElementById("edit-text-skills").hidden=true;
+    document.getElementById("edit-text-skills-ok").hidden=false;  
+}
+function edit_skillsOk(){
+    document.getElementById("text-skills").contentEditable=false;   
+    document.getElementById("edit-text-skills").hidden=false;
+    document.getElementById("edit-text-skills-ok").hidden=true;
+}
+function elim_skills(){
+    document.getElementById("skills").hidden=true;
+}
+/*----------------------------*/
+/* Funciones para editar Proyectos*/
+function edit_proyect(){
+    document.getElementById("text-proyect").contentEditable=true;   
+    document.getElementById("edit-text-proyect").hidden=true;
+    document.getElementById("edit-text-proyect-ok").hidden=false;  
+}
+function edit_proyectOk(){
+    document.getElementById("text-proyect").contentEditable=false;   
+    document.getElementById("edit-text-proyect").hidden=false;
+    document.getElementById("edit-text-proyect-ok").hidden=true;
+}
+function elim_proyect(){
+    document.getElementById("proyect").hidden=true;
+}
+/*----------------------------*/
